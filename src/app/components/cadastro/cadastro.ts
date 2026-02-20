e3403fec68f8ae8fb3e79b4c0d72be1eaed5d597
fabioalbuquerque <albuquerquerfabio833@gmail.com>
2026-02-19 22:42:16 -0300

    validador de erro

--- src/app/components/cadastro/cadastro.ts
+++ src/app/components/cadastro/cadastro.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
 export class Cadastro {
+ registerform: FormGroup;
+
+ constructor(private fb: FormBuilder) {
+   this.registerform = this.fb.group({
+     name: ['', [Validators.required]],
+     email: ['', [Validators.required, Validators.email]],
+     password: ['',[Validators.required, Validators.minLength(6)]],
+   });
+ }
+
+  onSubmit() {
+   // if( this.registerform.valid) {
+    //  alert('Cadastro realizado com sucesso!');
+   // }
+    console.log(this.registerform.value);
+  }
 
 }
