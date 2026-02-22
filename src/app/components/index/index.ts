4ac46cd6df6880d4e2c59f23fb5d84bb37612348
Fabioalbuqueque <albuquerquefabio833@gmail.com>
2026-02-22 16:56:56 -0300

    index

--- src/app/components/index/index.ts
+++ src/app/components/index/index.ts
@@ -1,13 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './index.html',
   styleUrl: './index.css',
 })
 export class Index {
+loginform: FormGroup;
 
-  
+constructor(private fb: FormBuilder) {
+   this.loginform = this.fb.group({
+     email: ['', [Validators.required, Validators.email]],
+     password: ['',[Validators.required, Validators.minLength(6)]],
+   });
+ }
+
+
+
+
+
+onSubmit() {  console.log(this.loginform.value);}
+
 }
